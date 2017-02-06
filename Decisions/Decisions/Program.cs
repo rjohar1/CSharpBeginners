239bd33dae3e53c654dd0276fc8a3ae6152d35ec
Rajeev <rjohar1@hotmail.com>
2017-02-06 13:38:03 -0800

    Initial Check in with message inside of if else block. Variable defined inside of each block.

---
+++ Decisions/Decisions/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bob's Big Giveaway");
+            Console.Write("Choose a door: 1, 2 or 3:");
+            string userValue = Console.ReadLine();
+
+            if (userValue == "1")
+            {
+                string message = "You won a new car";
+                Console.WriteLine(message);
+
+            }
+            else if (userValue == "2")
+            {
+                string message = "You won a new boat";
+                Console.WriteLine(message);
+            }
+            else if (userValue == "3")
+            {
+                string message = "You won a new cat";
+                Console.WriteLine(message);
+            }
+            else
+            {
+                string message = "Sorry, we didn't uundersstand.";
+                Console.WriteLine(message);
+            }
+
+           
+            Console.ReadLine();
+
+        }
+    }
+}
