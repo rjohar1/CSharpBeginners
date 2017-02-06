99a87bf7d0bbe1de096797fe9df7adc9ee3e1ab1
Rajeev <rjohar1@hotmail.com>
2017-02-06 12:50:05 -0800

    Finish HellowWorld project.

--- SampleCode/cs-code/Lesson_03/HelloWorld/HelloWorld/Program.cs
+++ SampleCode/cs-code/Lesson_03/HelloWorld/HelloWorld/Program.cs
@@ -10,8 +10,8 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();            
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
         }
     }
 }
