11b32eff2adec525070958c4e98b24b2b002084a
Rajeev <rjohar1@hotmail.com>
2017-02-06 13:57:30 -0800

    using {} to display variables.

--- Decisions/Decisions/Program.cs
+++ Decisions/Decisions/Program.cs
@@ -10,32 +10,51 @@ namespace Decisions
     {
         static void Main(string[] args)
         {
+            /*
+                Console.WriteLine("Bob's Big Giveaway");
+                Console.Write("Choose a door: 1, 2 or 3:");
+                string userValue = Console.ReadLine();
+
+                string message = "";
+
+                if (userValue == "1")
+                {
+                    message = "You won a new car";                
+
+                }
+                else if (userValue == "2")
+                {
+                    message = "You won a new boat";
+                }
+                else if (userValue == "3")
+                {
+                    message = "You won a new cat";
+                }
+                else
+                {
+                    message = "Sorry, we didn't uundersstand.";
+                    message += "You loose.";
+                }
+
+                Console.WriteLine(message);
+                Console.ReadLine();
+                */
+
             Console.WriteLine("Bob's Big Giveaway");
             Console.Write("Choose a door: 1, 2 or 3:");
             string userValue = Console.ReadLine();
 
-            string message = "";
+            string message = (userValue == "1") ? "boat" : "strand of lint";
 
-            if (userValue == "1")
-            {
-                message = "You won a new car";                
+            //Console.Write("You won a ");
+            //Console.Write(message);
+            //Console.Write(".");
 
-            }
-            else if (userValue == "2")
-            {
-                message = "You won a new boat";
-            }
-            else if (userValue == "3")
-            {
-                message = "You won a new cat";
-            }
-            else
-            {
-                message = "Sorry, we didn't uundersstand.";
-            }
 
-            Console.WriteLine(message);
+            //Console.WriteLine("You won a {0}. ", message);
+            Console.WriteLine("You entered: {0}, therefore you won a {1}. ", userValue, message);
             Console.ReadLine();
+
         }
     }
 }
