6b7757327da1c3eed3c1ad382d7d1802dbb44119
Rajeev <rjohar1@hotmail.com>
2017-02-08 11:06:34 -0800

    WorkingWithStringd

---
+++ WorkingWithStrings/WorkingWithStrings/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Formmatting Strings
+
+            //string myString = "My \"so-called\" life";
+            //string myString = "what if I need a \nnew line?";
+            //string myString = "Go to your c:\\ drive";
+            //string myString = @"Go to your c:\ drive";
+
+            // string myString = string.Format("{1} = {1}", "First", "Second");
+            // string myString = string.Format("{0:C}", 123.45);
+            //string myString = string.Format("{0:N}", 1234567890);
+            //string myString = string.Format("Percentage: {0:P}", .123);
+            //string myString = string.Format("Phone Number: {0:(###) ###-####}", 1234567889012);
+
+            // Maaaanipulating Strings
+
+            //string myString = " That summer we took threees acreoss the board ";
+
+            //myString = myString.Substring(6,14);
+            //myString = myString.ToUpper();
+            //myString = myString.Replace(" ", "--");
+            //myString = myString.Remove(6, 14);
+            //myString = String.Format("Length before {0} -- Length after: {1}", myString.Length, myString.Trim().Length);
+
+            /*
+            string myString = "";
+
+            for (int i = 0; i < 100; i++)
+            {
+                myString += "--" + i.ToString();
+            }
+            */
+
+            StringBuilder myString = new StringBuilder();
+
+            for (int i = 0; i < 100; i++)
+            {
+                myString.Append("--");
+                myString.Append(i);
+            }
+            Console.WriteLine(myString);
+            Console.ReadLine();
+        }
+    }
+}
