d6b474c331768f28a366c65e07a6834087c11aa8
Rajeev <rjohar1@hotmail.com>
2017-02-07 19:39:04 -0800

    Initail commit with ReverseString function

---
+++ SimpleMethod/SimpleMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Helloworld();
+            Console.ReadLine();
+        }
+
+        private static void Helloworld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
