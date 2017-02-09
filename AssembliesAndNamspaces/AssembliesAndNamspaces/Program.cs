1b18551d4ce6f82435e3b51f5511f02e6f317753
Rajeev <rjohar1@hotmail.com>
2017-02-09 08:18:42 -0800

    Initial commit (18)

---
+++ AssembliesAndNamspaces/AssembliesAndNamspaces/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace AssembliesAndNamspaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //// Example #2: Write one string to a text file.
+            //string text = "we want to write this to our file.";
+            //// WriteAllText creates a file, writes the specified string to the file,
+            //// and then closes the file.    You do NOT need to call Flush() or Close().
+            //File.WriteAllText(@"C:\Projects\CSharpBeginners\Lesson17\WriteText.txt", text);
+
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("http://msdn.microsoft.com");
+            File.WriteAllText(@"C:\Projects\CSharpBeginners\Lesson17\WriteText.txt", reply);
+
+
+            // using namespace
+            Console.WriteLine(reply);
+            Console.ReadLine();
+        }
+    }
+}
