1682e27b8f217ccbbc8977ccdd321a177bf604b0
Rajeev <rjohar1@hotmail.com>
2017-02-15 11:51:50 -0800

    Initial Commit

--- EnumAndSwitch/EnumAndSwitch/Program.cs
+++ EnumAndSwitch/EnumAndSwitch/Program.cs
@@ -28,6 +28,7 @@ namespace EnumsAndSwitch
                 new Todo { Description = "Task 14", EstimatedHours = 6, Status = Status.Completed }
             };
 
+            Console.ForegroundColor = ConsoleColor.DarkRed;
             PrintAssessment(todos);
             Console.ReadLine();
 
@@ -37,7 +38,27 @@ namespace EnumsAndSwitch
         {
             foreach (var todo in todos)
             {
-
+                switch (todo.Status)
+                {
+                    case Status.NotStarted:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+                    case Status.InProgress:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                    case Status.OnHold:
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        break;
+                    case Status.Completed:
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        break;
+                    case Status.Deleted:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                    default:
+                        break;
+                }
+                Console.WriteLine(todo.Description);
             }
         }
     }
