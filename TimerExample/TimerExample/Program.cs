4a5ed07562f0e5a2677c57c998124082931ebf94
Rajeev <rjohar1@hotmail.com>
2017-02-16 11:22:54 -0500

    Final Commit(24)

--- TimerExample/TimerExample/Program.cs
+++ TimerExample/TimerExample/Program.cs
@@ -11,6 +11,33 @@ namespace TimerExample
     {
         static void Main(string[] args)
         {
+            Timer myTimer = new Timer(2000);
+
+            // Attaching en event handler
+            myTimer.Elapsed += MyTimer_Elapsed;
+            myTimer.Elapsed += MyTimer_Elapsed1;
+
+            myTimer.Start();
+
+            Console.WriteLine("Press enter to remove the red event");
+            Console.ReadLine();
+
+            // Detach an event handler
+            myTimer.Elapsed -= MyTimer_Elapsed1;
+
+            Console.ReadLine();
+        }
+
+        private static void MyTimer_Elapsed1(object sender, ElapsedEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Elapsed Time: {0:HH:mm:ss.fff}", e.SignalTime);
+        }
+
+        private static void MyTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Elapsed Time: {0:HH:mm:ss.fff}", e.SignalTime);
         }
     }
 }
